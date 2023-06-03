014d3b1539807585e071b1d113d82f26829477f0
adamewing <adam.ewing@gmail.com>
2023-06-03 13:46:07 +0000

    additional annotation track mostly working

--- lrm_viewer.py
+++ lrm_viewer.py
@@ -2,7 +2,6 @@
 
 import os
 import sys
-import gzip
 import argparse
 from collections import defaultdict as dd
 
@@ -14,7 +13,6 @@ import plotly.express as px
 from dash.dependencies import Input, Output, State
 
 from bx.intervals.intersection import Intersecter, Interval
-import pysam
 
 import logging
 FORMAT = '%(asctime)s %(message)s'
@@ -58,69 +56,12 @@ def load_metrics(plotdir):
     return genome_len, target_len
 
 
-def load_gtf(gtf, gchrom, gstart, gend):
-    logger.info(f'gtf segment load {gchrom}:{gstart}-{gend}...')
-    gstart = int(gstart)
-    gend   = int(gend)
-
-    gtf_tx    = dd(Intersecter)
-    gtf_exons = dd(Intersecter)
-    gtf_utrs  = dd(Intersecter)
-
-    if gtf is None:
-        return gtf_tx, gtf_exons, gtf_utrs
-
-    if gchrom not in gtf.contigs:
-        return gtf_tx, gtf_exons, gtf_utrs
-
-    for line in gtf.fetch(gchrom, gstart, gend):
-        if line.startswith('#'):
-            continue
-
-        chrom, source, feature, start, end, score, strand, frame, attribs = line.split('\t')        
-
-        start = int(start)
-        end = int(end)
-        attribs = attribs.strip()
-
-        attr_dict = {}
-
-        for attrib in attribs.split(';'):
-            if attrib:
-                key, val = attrib.strip().split()[:2]
-                key = key.strip()
-                val = val.strip().strip('"')
-                attr_dict[key] = val
-
-        if 'gene_id' not in attr_dict:
-            continue
-
-        if 'gene_name' not in attr_dict:
-            attr_dict['gene_name'] = attr_dict['gene_id']
-
-        if 'transcript_id' not in attr_dict:
-            continue
-
-        if 'transcript_name' not in attr_dict:
-            attr_dict['transcript_name'] = attr_dict['transcript_id']
-
-        name = attr_dict['gene_name']
-        tx = attr_dict['transcript_name']
-
-        if feature == 'exon':
-            gtf_exons[chrom].add_interval(Interval(start, end, value=f'{tx}:{name}'))
-        
-        if feature == 'transcript':
-            gtf_tx[chrom].add_interval(Interval(start, end, value=f'{tx}:{name}'))
-
-        if feature in ('three_prime_utr','five_prime_utr'):
-            gtf_utrs[chrom].add_interval(Interval(start, end, value=f'{tx}:{name}'))
-
-    return gtf_tx, gtf_exons, gtf_utrs
-
-
 def load_targets(bed_fn):
+    logger.info(f'loading targets from {bed_fn}')
     targets = dd(Intersecter)
+
+    if bed_fn is None:
+        return targets
 
     with open(bed_fn) as bed:
         for line in bed:
@@ -129,21 +70,21 @@ def load_targets(bed_fn):
             start = int(start)
             end = int(end)
 
-            targets[chrom].add_interval(Interval(start, end))
+            value = None
+            if len(c) > 3:
+                value = c[3]
+
+            targets[chrom].add_interval(Interval(start, end, value=value))
     
     return targets
 
 
-def dash_lrt(plotdir, target_bed):
+def dash_lrt(plotdir, target_bed, tx_bed):
     if not os.path.exists(plotdir):
         sys.exit(f'directory not found: {plotdir}')
     
     targets = load_targets(target_bed)
-
-    gtf = None
-
-    if args.gtf:
-        gtf = pysam.Tabixfile(args.gtf)
+    tx = load_targets(tx_bed)
 
     # Define the initial browser view
     initial_chr = 'chr19'
@@ -166,7 +107,7 @@ def dash_lrt(plotdir, target_bed):
     app = dash.Dash(__name__)
 
     app.layout = html.Div([
-        html.H3(id='title', children='Live Read Monitor for ONT output'),
+        html.H3(id='title', children=f'Live Read Monitor for ONT output: {os.path.basename(plotdir)}'),
         html.Div(id='metrics', children=f'genome size: {genome_len} | target size: {target_len} ({tgt_pct}%)'),
         html.H4(id='genome-desc', children=f'Enrichment Browser:'),
         html.Div([
@@ -175,7 +116,7 @@ def dash_lrt(plotdir, target_bed):
                 type='text',
                 value=f'{initial_chr}:{initial_start}-{initial_end}'
             ),
-            html.Button('Submit', id='submit-button', n_clicks=0)
+            html.Button('Submit', id='submit-button', n_clicks=0),
         ]),
         dcc.Graph(id='scatter-plot', figure=scatter_fig, config={'scrollZoom': True}),
         dcc.Graph(id='violin-plot', style={'display': 'inline-block'}),
@@ -233,24 +174,29 @@ def dash_lrt(plotdir, target_bed):
             y=df_view['log10_length'],
             mode='markers',
             marker=dict(
-                color=df_view['targeted'].apply(lambda x: 'red' if x == 'Y' else 'blue')
+                color=df_view['targeted'].apply(lambda x: 'red' if x == 'Y' else 'blue'),
+                opacity=0.5
             )
         ))
 
         scatter_fig.update_layout(
             xaxis_title="Genome Position (green = targets)",
             yaxis_title="Log Read Length",
+            height=600,
+            paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(0,0,0,0)',
             dragmode="pan",
             autosize=True,
         )
 
         scatter_fig.update_xaxes(range=[df_view['start'].min(), df_view['end'].max()])
-        scatter_fig.update_yaxes(range=[df_view['log10_length'].min()-1.0, df_view['log10_length'].max()+.5], fixedrange=True)
+        scatter_fig.update_yaxes(range=[df_view['log10_length'].min()-1.2, df_view['log10_length'].max()+.2], fixedrange=True)
 
         # draw targets
         
-        track_ymax = min(df_view['log10_length']) - 0.25
-        track_ymin = track_ymax- 0.5
+        tgt_track_ymax = max(df_view['log10_length']) + 0.1
+        tgt_track_ymin = min(df_view['log10_length']) - 0.1
+        
 
         target_boxes= []
         for rec in targets[chr].find(start, end):
@@ -259,39 +205,63 @@ def dash_lrt(plotdir, target_bed):
                 'xref': 'x',
                 'yref': 'y',
                 'x0': rec.start,
-                'y0': track_ymax,  # adjust y0 and y1 according to the desired track position
+                'y0': tgt_track_ymax,
                 'x1': rec.end,
-                'y1': track_ymin,
-                'fillcolor': 'green',  # adjust color and opacity as desired
-                'opacity': 0.5,
+                'y1': tgt_track_ymin,
+                'fillcolor': 'green',
+                'opacity': 0.2,
                 'line': {
                     'width': 0,
                 },
             })
         
 
-        # GTF track
+        # transcript track
     
-        gtf_track_ymax = min(df_view['log10_length']) - 0.5
-        gtf_track_ymin = track_ymax - 1.0
+        tx_track_ymax = tgt_track_ymin - 0.1
+        tx_track_ymin = tx_track_ymax - 0.8
+        tx_track_height = tx_track_ymax-tx_track_ymin
 
-        test_y = (gtf_track_ymax+gtf_track_ymin)/2
-        print(test_y)
+        stacklevel = {} # avoid gene overlaps
 
-        gtf_tx, gtf_exons, gtf_utrs = load_gtf(gtf, chr, start, end)
+        for rec in tx[chr].find(start, end):
+            overlaps = []
+            for o_rec in tx[chr].find(rec.start, rec.end):
+                if o_rec.value not in overlaps:
+                    overlaps.append(o_rec.value)
 
-        for rec in gtf_tx[chr].find(start, end):
+            for i, gene_name in enumerate(overlaps):
+                stacklevel[gene_name] = i
+
+        max_stacklevel = max(stacklevel.values())
+
+        tx_item_height = tx_track_height/(max_stacklevel+1)
+
+        font_vis = 'rgba(0,0,0,0)'
+
+        if end-start < 1e6:
+            font_vis = 'black'
+
+        for rec in tx[chr].find(start, end):
+            sl = stacklevel[rec.value]
+            y0 = tx_track_ymax - tx_item_height*sl
+            y1 = y0 - tx_item_height
             target_boxes.append({
                 'type': 'rect',
                 'xref': 'x',
                 'yref': 'y',
                 'x0': rec.start,
+                'y0': y0,
                 'x1': rec.end,
-                'y0': test_y-.1,
-                'y1': test_y+.1,
-                'fillcolor': 'black',
+                'y1': y1,
+                'fillcolor': 'orange',
+                'opacity': 0.5,
                 'line': {
-                    'width': 3,
+                    'width': 1,
+                },
+                'label': {
+                    'text': rec.value,
+                    'font': {'color': font_vis}
                 },
             })
 
@@ -349,7 +319,6 @@ def dash_lrt(plotdir, target_bed):
         max_e = int(df_enrich['enrichment'].max()+1)
         fig.update_layout(yaxis_range=[0,max_e])
 
-
         return fig
 
     if args.external:
@@ -363,9 +332,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='live coverage monitor aimed at fastqs generated by ONT devices')
     parser.add_argument('-d', '--plotdir', required=True, help='output directory')
     parser.add_argument('-t', '--targets', required=True, help='target .bed')
-    parser.add_argument('-g', '--gtf', default=None, help='GTF .gz (Ensembl works), tabix-indexed, chromosome names must match --targets, ref genome, etc')
+    parser.add_argument('--tx', default=None, help='transcripts (or any other annotation) .bed')
     parser.add_argument('--external', action='store_true', default=False)
     args = parser.parse_args()
-    dash_lrt(args.plotdir, args.targets)
+    dash_lrt(args.plotdir, args.targets, args.tx)
     
 
